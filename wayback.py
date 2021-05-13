84f97e308dca17f7e2173f989d75066bce7c3b2d
owu <83254308+owu-1@users.noreply.github.com>
2021-05-14 09:11:18 +1000

    Use date from current time to fetch snapshots
    #3
    Temp disable skipping search. Need to cache search options

--- wayback.py
+++ wayback.py
@@ -1,55 +1,62 @@
 def wayback(sources, download_function):
     import json
-    from datetime import datetime
+    from datetime import datetime, timedelta
     import os
-    import re
+    # import re
 
     CACHE_LOCATION = "snapshot_cache"
-    CACHE_REGEX = re.compile(r"^(\d{14})\.html$")
+    # CACHE_REGEX = re.compile(r"^(\d{14})\.html$")
     TIMESTAMP_FORMAT = "%Y%m%d%H%M%S"
     CDX_API_URL = ("http://web.archive.org/cdx/search/cdx?"
-                   "url={url}&output=json&fl=timestamp,digest&limit=-{limit}")
+                   "output=json&"  # return in json form
+                   "fl=timestamp&"  # only return timestamps
+                   "collapse=digest&"  # only show unique captures
+                   "url={url}&"
+                   "from={requested_date}&"
+                   "to={current_date}")
     SNAPSHOT_URL = "http://web.archive.org/web/{timestamp}id_/{url}"
 
+    current_date = datetime.now()
+    current_date_str = current_date.strftime(TIMESTAMP_FORMAT)
     parse_fail_timestamps = {}
     for source_name, source_data in sources.items():
         url = source_data["url"]
-        snapshot_search_limit = source_data["snapshot_search_limit"]
-        parse_function = source_data["parse_function"]
-        if snapshot_search_limit < 0:
-            print("Snapshot search limit is less than 0 for", source_name)
-            return
+        date_filter = source_data["date_filter"]
+        if isinstance(date_filter, int):
+            requested_date = current_date - timedelta(days=date_filter)
+            requested_date_str = requested_date.strftime(TIMESTAMP_FORMAT)
+        else:
+            requested_date_str = date_filter
+
         source_cache_dir = f"{CACHE_LOCATION}/{source_name}"
         os.makedirs(source_cache_dir, exist_ok=True)
 
         snapshot_timestamps = set()
-        if snapshot_search_limit == 0:
-            # use cached snapshots
-            cache_file_names = os.listdir(source_cache_dir)
-            for cache_file_name in cache_file_names:
-                match = CACHE_REGEX.match(cache_file_name)
-                if match:
-                    timestamp = match[1]
-                    snapshot_timestamps.add(timestamp)
-        else:
-            # search for unique snapshots
-            search_url = CDX_API_URL.format(url=url,
-                                            limit=snapshot_search_limit)
-            search_json = download_function(search_url)
-            snapshots = json.loads(search_json)
-            snapshots.pop(0)  # remove cdx format line
-            previous_digest = None
-            # multiple snapshots can occur at a single timestamp
-            for snapshot in snapshots:
-                timestamp = snapshot[0]
-                digest = snapshot[1]
-                # skip download if identical to previous snapshot
-                if digest == previous_digest:
-                    continue
-                snapshot_timestamps.add(timestamp)
-                previous_digest = digest
-            print("Found", len(snapshot_timestamps),
-                  "unique snapshots for", source_name)
+
+        # # use cached snapshots
+        # cache_file_names = os.listdir(source_cache_dir)
+        # for cache_file_name in cache_file_names:
+        #     match = CACHE_REGEX.match(cache_file_name)
+        #     if match:
+        #         timestamp = match[1]
+        #         snapshot_timestamps.add(timestamp)
+
+        # search for unique snapshots
+        search_url = CDX_API_URL.format(
+            url=url,
+            requested_date=requested_date_str,
+            current_date=current_date_str)
+        print(search_url)
+        search_json = download_function(search_url)
+        print(search_json)
+        snapshots = json.loads(search_json)
+        snapshots.pop(0)  # remove cdx format line
+        # multiple snapshots can occur at a single timestamp
+        for snapshot in snapshots:
+            timestamp = snapshot[0]
+            snapshot_timestamps.add(timestamp)
+        print("Found", len(snapshot_timestamps),
+              "unique snapshots for", source_name)
 
         # download, cache and test snapshots
         parse_fail_timestamps[source_name] = []
@@ -63,14 +70,15 @@ def wayback(sources, download_function):
                     snapshot_html = f.read()
             else:
                 print("Downloading snapshot from", timestamp_readable)
-                snapshot_url = SNAPSHOT_URL.format(timestamp=timestamp,
-                                                   url=url)
+                snapshot_url = SNAPSHOT_URL.format(
+                    timestamp=timestamp,
+                    url=url)
                 snapshot_html = download_function(snapshot_url)
                 # cache snapshot
                 with open(snapshot_location, 'w') as f:
                     f.write(snapshot_html)
             # test snapshot
-            parse_result = parse_function(snapshot_html)
+            parse_result = source_data["parse_function"](snapshot_html)
             if parse_result:
                 print("Parsed successfully!")
             else:
