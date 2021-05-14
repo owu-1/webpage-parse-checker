2eef3df6bdd1f361045feb6b636038d047d7c437
owu <83254308+owu-1@users.noreply.github.com>
2021-05-14 23:24:06 +1000

    Refactor

--- wayback.py
+++ wayback.py
@@ -3,12 +3,12 @@ def wayback(sources, download_function):
     from datetime import datetime, timedelta
     import os
     import re
-    from pprint import pprint
 
     CACHE_LOCATION = "snapshot_cache"
     CACHE_REGEX = re.compile(r"^(\d{14})\.html$")
     SETTINGS_LOCATION = f"{CACHE_LOCATION}/settings.json"
     PARSE_RESULTS_LOCATION = f"{CACHE_LOCATION}/parse_results.json"
+    CHANGES_LOCATION = f"{CACHE_LOCATION}/changes.json"
     CACHE_TIME = timedelta(hours=1)
     TIMESTAMP_FORMAT = "%Y%m%d%H%M%S"
     CDX_API_URL = ("http://web.archive.org/cdx/search/cdx?"
@@ -17,9 +17,123 @@ def wayback(sources, download_function):
                    "collapse=digest&"  # only show unique captures
                    "filter=statuscode:200&"  # only return pages without errors
                    "url={url}&"
-                   "from={requested_date}&"
+                   "from={date_filter}&"
                    "to={current_date}")
     SNAPSHOT_URL = "http://web.archive.org/web/{timestamp}id_/{url}"
+
+    class Source:
+        def __init__(self, name, data, current_date):
+            self.name = name
+            self.url = data["url"]
+            self.parse_function = data["parse_function"]
+
+            date_filter = data["date_filter"]
+            # use day offset if integer, or wayback date format if string
+            if isinstance(date_filter, int):
+                self.date_filter = current_date - timedelta(days=date_filter)
+            else:
+                self.date_filter = datetime.strptime(date_filter,
+                                                     TIMESTAMP_FORMAT)
+            # create source cache directory
+            self.cache_dir = f"{CACHE_LOCATION}/{self.name}"
+            os.makedirs(self.cache_dir, exist_ok=True)
+
+        def download_snapshots(self, settings, current_date):
+            # check if snapshots are cached
+            oldest_date_filter_str = settings[self.name].get("date_filter")
+            downloaded_date_str = settings[self.name].get("downloaded_date")
+            if oldest_date_filter_str and downloaded_date_str:
+                oldest_date_filter = datetime.strptime(oldest_date_filter_str,
+                                                       TIMESTAMP_FORMAT)
+                downloaded_date = datetime.strptime(downloaded_date_str,
+                                                    TIMESTAMP_FORMAT)
+                if (oldest_date_filter <= self.date_filter or
+                        (current_date - downloaded_date) > CACHE_TIME):
+                    # use cached snapshots
+                    cache_file_names = os.listdir(self.cache_dir)
+                    timestamps = []
+                    for cache_file_name in cache_file_names:
+                        match = CACHE_REGEX.match(cache_file_name)
+                        if match:
+                            timestamp = match[1]
+                            timestamps.append(timestamp)
+                    print("Found", len(timestamps),
+                          "snapshots cached for", self.name)
+                    return timestamps
+
+            # retrive timestamps
+            search_url = CDX_API_URL.format(
+                url=self.url,
+                date_filter=self.date_filter.strftime(TIMESTAMP_FORMAT),
+                current_date=current_date.strftime(TIMESTAMP_FORMAT)
+            )
+            search_json = download_function(search_url)
+            snapshots = json.loads(search_json)
+            if not snapshots:
+                print("No snapshots found for", self.name)
+                return []
+            snapshots.pop(0)  # remove cdx format line
+            timestamps = set()  # multiple snapshots can occur at one timestamp
+            for snapshot in snapshots:
+                timestamp = snapshot[0]
+                timestamps.add(timestamp)
+            print("Found", len(timestamps),
+                  "unique snapshots for", self.name)
+            # download and cache snapshots
+            for timestamp in timestamps:
+                snapshot_location = f"{self.cache_dir}/{timestamp}.html"
+                if os.path.isfile(snapshot_location):
+                    continue
+                else:
+                    print("Downloading snapshot from",
+                          datetime.strptime(timestamp, TIMESTAMP_FORMAT))
+                    snapshot_url = SNAPSHOT_URL.format(
+                        timestamp=timestamp,
+                        url=self.url)
+                    snapshot_html = download_function(snapshot_url)
+                    # cache snapshot
+                    with open(snapshot_location, 'w') as f:
+                        f.write(snapshot_html)
+            settings[source_name]["date_filter"] = \
+                self.date_filter.strftime(TIMESTAMP_FORMAT)
+            settings[source_name]["downloaded_date"] = \
+                current_date.strftime(TIMESTAMP_FORMAT)
+            return timestamps
+
+        def parse_snapshots(self, timestamps, all_previous_results):
+            if all_previous_results:
+                previous_results = all_previous_results[self.name]
+            else:
+                previous_results = None
+
+            results = {}
+            fails = []
+            changes = []
+            for timestamp in timestamps:
+                snapshot_location = f"{self.cache_dir}/{timestamp}.html"
+                with open(snapshot_location) as f:
+                    snapshot_html = f.read()
+                result = source_data["parse_function"](snapshot_html)
+                if result:
+                    print("Parsed successfully!")
+                    if isinstance(result, dict):
+                        results[timestamp] = result
+                        if previous_results:
+                            previous_result = previous_results.get(timestamp)
+                            if (previous_result and
+                                    not result == previous_result):
+                                change = {
+                                    "timestamp": timestamp,
+                                    "previous": previous_result,
+                                    "current": result
+                                }
+                                changes.append(change)
+                    else:
+                        results[timestamp] = None
+                else:
+                    print("Parse failed.")
+                    fails.append(timestamp)
+            return (results, fails, changes)
 
     # open settings file
     if os.path.isfile(SETTINGS_LOCATION):
@@ -34,132 +148,43 @@ def wayback(sources, download_function):
     # open previous parse results file
     if os.path.isfile(PARSE_RESULTS_LOCATION):
         with open(PARSE_RESULTS_LOCATION) as f:
-            previous_parse_results = json.load(f)
+            previous_results = json.load(f)
     else:
-        previous_parse_results = None
+        previous_results = None
 
     current_date = datetime.now()
-    current_date_str = current_date.strftime(TIMESTAMP_FORMAT)
-    parse_fail_timestamps = {}
-    parse_results = {}
+    all_results = {}
+    all_fails = {}
+    all_changes = {}
     for source_name, source_data in sources.items():
-        url = source_data["url"]
-        date_filter = source_data["date_filter"]
-        # create source cache directory
-        source_cache_dir = f"{CACHE_LOCATION}/{source_name}"
-        os.makedirs(source_cache_dir, exist_ok=True)
-        # use day offset if integer, or wayback date format if string
-        if isinstance(date_filter, int):
-            requested_date = current_date - timedelta(days=date_filter)
-            requested_date_str = requested_date.strftime(TIMESTAMP_FORMAT)
-        else:
-            requested_date = datetime.strptime(date_filter, TIMESTAMP_FORMAT)
-            requested_date_str = date_filter
+        source = Source(source_name, source_data, current_date)
+        timestamps = source.download_snapshots(settings, current_date)
+        results, fails, changes = source.parse_snapshots(timestamps,
+                                                         previous_results)
+        all_results[source_name] = results
+        all_fails[source_name] = fails
+        all_changes[source_name] = changes
 
-        # check settings
-        oldest_date_filter_str = settings[source_name].get("date_filter")
-        if oldest_date_filter_str:
-            oldest_date_filter = datetime.strptime(oldest_date_filter_str,
-                                                   TIMESTAMP_FORMAT)
-            if (oldest_date_filter - requested_date) < CACHE_TIME:
-                search = False
-            else:
-                search = True
-        else:
-            search = True
-
-        # retrive timestamps
-        timestamps = set()  # multiple snapshots can occur at one timestamp
-        if search:
-            # search for unique snapshots
-            search_url = CDX_API_URL.format(
-                url=url,
-                requested_date=requested_date_str,
-                current_date=current_date_str)
-            search_json = download_function(search_url)
-            snapshots = json.loads(search_json)
-            snapshots.pop(0)  # remove cdx format line
-
-            for snapshot in snapshots:
-                timestamp = snapshot[0]
-                timestamps.add(timestamp)
-            print("Found", len(timestamps),
-                  "unique snapshots for", source_name)
-
-            # download and cache snapshots
-            for timestamp in timestamps:
-                snapshot_location = f"{source_cache_dir}/{timestamp}.html"
-                timestamp_readable = datetime.strptime(timestamp,
-                                                       TIMESTAMP_FORMAT)
-                if os.path.isfile(snapshot_location):
-                    continue
-                else:
-                    print("Downloading snapshot from", timestamp_readable)
-                    snapshot_url = SNAPSHOT_URL.format(
-                        timestamp=timestamp,
-                        url=url)
-                    snapshot_html = download_function(snapshot_url)
-                    # cache snapshot
-                    with open(snapshot_location, 'w') as f:
-                        f.write(snapshot_html)
-        else:
-            # use cached snapshots
-            cache_file_names = os.listdir(source_cache_dir)
-            for cache_file_name in cache_file_names:
-                match = CACHE_REGEX.match(cache_file_name)
-                if match:
-                    timestamp = match[1]
-                    timestamps.add(timestamp)
-            print("Found", len(timestamps),
-                  "snapshots cached for", source_name)
-
-        # test snapshots
-        source_parse_results = {}
-        if previous_parse_results:
-            previous_source_parse_results = previous_parse_results[source_name]
-        parse_fail_timestamps[source_name] = []
-        fails = parse_fail_timestamps[source_name]
-        for timestamp in timestamps:
-            snapshot_location = f"{source_cache_dir}/{timestamp}.html"
-            with open(snapshot_location) as f:
-                snapshot_html = f.read()
-            parse_result = source_data["parse_function"](snapshot_html)
-            if parse_result:
-                print("Parsed successfully!")
-                if isinstance(parse_result, dict):
-                    source_parse_results[timestamp] = parse_result
-                    if previous_parse_results:
-                        previous_parse_result = \
-                            previous_source_parse_results.get(timestamp)
-                        if (previous_parse_result and
-                                not parse_result == previous_parse_result):
-                            print("Parse result is different "
-                                  "from previous result for", timestamp)
-                            print("Previous:")
-                            pprint(previous_parse_result)
-                            print("Current:")
-                            pprint(parse_result)
-                else:
-                    source_parse_results[timestamp] = None
-            else:
-                print("Parse failed.")
-                fails.append(timestamp)
-
-        # update parse results
-        parse_results[source_name] = source_parse_results
-
-        # update date filter setting
-        if search:
-            settings[source_name]["date_filter"] = requested_date_str
-            with open(SETTINGS_LOCATION, 'w') as f:
-                json.dump(settings, f)
+    # update date filter setting
+    with open(SETTINGS_LOCATION, 'w') as f:
+        json.dump(settings, f)
 
     # save parse results
     with open(PARSE_RESULTS_LOCATION, 'w') as f:
-        json.dump(parse_results, f)
+        json.dump(all_results, f)
 
-    # failed parses overview
-    for source_name, timestamps in parse_fail_timestamps.items():
-        print(source_name, "had", len(timestamps), "parse fails")
-        if timestamps:
-            print("Parse fails occured in snapshots from:", *timestamps)
+    # save changes
+    with open(CHANGES_LOCATION, 'w') as f:
+        json.dump(all_changes, f)
+
+    # overviews
+    for source_name in sources:
+        fails = all_fails[source_name]
+        changes = all_changes[source_name]
+        print(source_name, "had", len(fails), "parse fails")
+        if fails:
+            print("Parse fails occured in snapshots from:", *fails)
+        print(source_name, "had", len(changes),
+              "different results to previous results")
+        if changes:
+            print("See changes in", CHANGES_LOCATION)
